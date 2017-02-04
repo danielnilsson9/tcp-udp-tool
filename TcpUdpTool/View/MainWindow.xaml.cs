235eaf27806e1ed05030a6fe984858aeaea54ea2
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2017-02-04 12:21:42 +0100

    fix mistake

--- TcpUdpTool/View/MainWindow.xaml.cs
+++ TcpUdpTool/View/MainWindow.xaml.cs
@@ -1,23 +1,17 @@
 ﻿using System.Windows;
-using TcpUdpTool.ViewModel;
 
-namespace TcpUdpTool
+namespace TcpUdpTool.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
-            //Closing += MainWindow_Closing;
         }
 
-        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            Properties.Settings.Default.Save();
-        }
     }
 }
